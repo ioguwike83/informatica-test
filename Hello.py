bf33ca85ac1a7b5b8a61893418c3a42539323cff
ioguwike83 <ioguwike83@yahoo.com>
2021-06-17 18:41:53 -0600

    testing repo

---
+++ Hello.py
@@ -0,0 +1,12 @@
+list_name = ["akhigbe", "mark", "jude"]
+
+
+def Cap_name(object_list):
+    for item in object_list:
+        value = []
+        value.append(item.upper())
+        return value
+
+
+Cap_name(list_name)        
+    
